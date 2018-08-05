5227ec53b49846e6ead63f8024271558b4653f18
DG Developers <dgdevelopers@hotmail.com>
2018-08-05 08:57:01 -0700

    changed server address & minor updates

--- app/src/main/java/com/dgdev/mbtms/remote/ApiUtils.java
+++ app/src/main/java/com/dgdev/mbtms/remote/ApiUtils.java
@@ -4,9 +4,10 @@ public class ApiUtils {
     public ApiUtils() {
     }
 
-    public static String BASE_URL = "http://192.168.137.1/mobileapi/";
+    //public static String BASE_URL = "http://icds.000webhostapp.com/mobileapi/";
+    public static String BASE_URL = "http://v2.icdsjalpaiguri.in/mobileapi/";
 
-    public static  APIInterface getAPIService(){
+    public static APIInterface getAPIService() {
         return ApiClient.getClient(BASE_URL).create(APIInterface.class);
     }
 }
