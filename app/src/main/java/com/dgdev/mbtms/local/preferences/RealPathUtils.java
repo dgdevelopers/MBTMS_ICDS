5364589fe509271595668b3ec42437a28eff093a
DG Developers <dgdevelopers@hotmail.com>
2018-08-08 10:00:53 +0530

    marshmellow and above support added...

--- app/src/main/java/com/dgdev/mbtms/local/preferences/RealPathUtils.java
+++ app/src/main/java/com/dgdev/mbtms/local/preferences/RealPathUtils.java
@@ -4,35 +4,120 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v4.content.CursorLoader;
+
+import java.io.File;
 
 public class RealPathUtils {
 
     @SuppressLint("NewApi")
     public static String getRealPathFromURI_API19(Context context, Uri uri){
         String filePath = "";
-        String wholeID = DocumentsContract.getDocumentId(uri);
 
-        // Split at colon, use second item in the array
-        String id = wholeID.split(":")[1];
+        // ExternalStorageProvider
+        if (isExternalStorageDocument(uri)) {
+            final String docId = DocumentsContract.getDocumentId(uri);
+            final String[] split = docId.split(":");
+            final String type = split[0];
 
-        String[] column = { MediaStore.Images.Media.DATA };
+            if ("primary".equalsIgnoreCase(type)) {
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            } else {
 
-        // where id is equal to
-        String sel = MediaStore.Images.Media._ID + "=?";
+                if (Build.VERSION.SDK_INT > 20) {
+                    //getExternalMediaDirs() added in API 21
+                    File extenal[] = context.getExternalMediaDirs();
+                    if (extenal.length > 1) {
+                        filePath = extenal[1].getAbsolutePath();
+                        filePath = filePath.substring(0, filePath.indexOf("Android")) + split[1];
+                    }
+                }else{
+                    filePath = "/storage/" + type + "/" + split[1];
+                }
+                return filePath;
+            }
 
-        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                column, sel, new String[]{ id }, null);
+        } else if (isDownloadsDocument(uri)) {
+            // DownloadsProvider
+            final String id = DocumentsContract.getDocumentId(uri);
+            //final Uri contentUri = ContentUris.withAppendedId(
+            // Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
 
-        int columnIndex = cursor.getColumnIndex(column[0]);
+            Cursor cursor = null;
+            final String column = "_data";
+            final String[] projection = {column};
 
-        if (cursor.moveToFirst()) {
-            filePath = cursor.getString(columnIndex);
+            try {
+                cursor = context.getContentResolver().query(uri, projection, null, null, null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    final int index = cursor.getColumnIndexOrThrow(column);
+                    String result = cursor.getString(index);
+                    cursor.close();
+                    return result;
+                }
+            } finally {
+                if (cursor != null)
+                    cursor.close();
+            }
+        } else if (DocumentsContract.isDocumentUri(context, uri)) {
+            // MediaProvider
+            String wholeID = DocumentsContract.getDocumentId(uri);
+
+            // Split at colon, use second item in the array
+            String[] ids = wholeID.split(":");
+            String id;
+            String type;
+            if (ids.length > 1) {
+                id = ids[1];
+                type = ids[0];
+            } else {
+                id = ids[0];
+                type = ids[0];
+            }
+
+            Uri contentUri = null;
+            if ("image".equals(type)) {
+                contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            } else if ("video".equals(type)) {
+                contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+            } else if ("audio".equals(type)) {
+                contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+            }
+
+            final String selection = "_id=?";
+            final String[] selectionArgs = new String[]{id};
+            final String column = "_data";
+            final String[] projection = {column};
+            Cursor cursor = context.getContentResolver().query(contentUri,
+                    projection, selection, selectionArgs, null);
+
+            if (cursor != null) {
+                int columnIndex = cursor.getColumnIndex(column);
+
+                if (cursor.moveToFirst()) {
+                    filePath = cursor.getString(columnIndex);
+                }
+                cursor.close();
+            }
+            return filePath;
+        } else {
+            String[] proj = {MediaStore.Audio.Media.DATA};
+            Cursor cursor = context.getContentResolver().query(uri, proj, null, null, null);
+            if (cursor != null) {
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+                if (cursor.moveToFirst())
+                    filePath = cursor.getString(column_index);
+                cursor.close();
+            }
+
+
+            return filePath;
         }
-        cursor.close();
-        return filePath;
+        return null;
     }
 
 
@@ -62,6 +147,15 @@ public class RealPathUtils {
                 = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
         cursor.moveToFirst();
         return cursor.getString(column_index);
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
     }
 }
 
